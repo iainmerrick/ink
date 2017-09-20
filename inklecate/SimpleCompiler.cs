87defb2e35a4ba81b6af6d96b6253d7649ad8e99
Iain Merrick <iain@iainmerrick.com>
2017-09-20 15:17:31 -0500

    Add SimpleCompiler.cs

---
+++ inklecate/SimpleCompiler.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System.Text;
+
+namespace Ink
+{
+	public static class SimpleCompiler
+	{
+		public static Runtime.Story Compile(string path, ErrorHandler errorHandler)
+		{
+			var text = File.ReadAllText(path, Encoding.UTF8);
+			var name = Path.GetFileName(path);
+
+			string oldDir = Directory.GetCurrentDirectory();
+			try
+			{
+				var dir = Path.GetDirectoryName(path);
+				Directory.SetCurrentDirectory(dir);
+				return Compile(text, name, errorHandler);
+			}
+			finally
+			{
+				Directory.SetCurrentDirectory(oldDir);
+			}
+		}
+
+		public static Runtime.Story Compile(string text, string name, ErrorHandler errorHandler)
+		{
+			var parser = new InkParser(text, name, errorHandler);
+			var result = parser.Parse();
+			var story = result.ExportRuntime(errorHandler);
+			return story;
+		}
+	}
+}
